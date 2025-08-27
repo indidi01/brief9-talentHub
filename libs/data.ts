5c6d58e5980365c5a32b35eceb03cb18f813e1cd
indidi <indian.v01@gmail.com>
2025-08-27 16:43:40 +0200

    push obligatoire

---
+++ libs/data.ts
@@ -0,0 +1,5 @@
+public const annonces = [
+  { id: 1, titre: 'Développeur(se) junior TS React', description: 'Lorem ipsum...', date: 'Il y a 2 jours' },
+  { id: 2, titre: 'Backend junior PHP Symfony', description: 'Lorem ipsum...', date: 'Il y a 5 jours' },
+  { id: 3, titre: 'Data Scientist Python', description: 'Lorem ipsum...', date: 'Il y a 1 semaine' },
+];
---
+++ routes/globals.ts
@@ -0,0 +1,5 @@
+const router = router();
+
+router.get("/", (request, response) => {
+    
+})
